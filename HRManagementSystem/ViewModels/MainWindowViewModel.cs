fa035da432fd7c060cd78b344413e498c1a65f15
lightereb <lightereb@126.com>
2025-02-25 11:32:21 +0800

    初始化项目

---
+++ HRManagementSystem/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ReactiveUI;
+
+namespace HRManagementSystem.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private object? _currentPage;
+        
+        public object? CurrentPage
+        {
+            get => _currentPage;
+            set => this.RaiseAndSetIfChanged(ref _currentPage, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            // 初始化
+        }
+    }
+}
---
+++ HRManagementSystem/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HRManagementSystem.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
